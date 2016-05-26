157beb5314f7be2e47d6ab3cf5c99bd832ce4dde
Wojtek Siudzinski <admin@suda.pl>
2016-05-26 17:38:41 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+// Counter
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+
+// jQuery Internal Animation Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
